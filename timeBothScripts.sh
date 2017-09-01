89972a3561239f652ed1cd93f2d946eb2ef0a4df
Monty Williams <monty.williams@gmail.com>
2017-09-01 15:53:02 -0700

    Imporove readability of log files

--- timeBothScripts.sh
+++ timeBothScripts.sh
@@ -2,25 +2,25 @@
 
 # Time the primary variants of shell scripts that make spreadsheets
 
+echo "========================================"
 echo "==> time ./makeMHzSpreadsheet.sh -td"
 date
 time ./makeMHzSpreadsheet.sh -td
 echo ""
-echo "---"
-echo ""
+echo "----------------------------------------"
 echo "==> time ./makeMHzSpreadsheet.sh -tld"
 date
 time ./makeMHzSpreadsheet.sh -tld
 echo ""
-echo "---"
-echo ""
 
+echo "----------------------------------------"
 echo "==> time ./makeAcornSpreadsheet.sh -td"
 date
 time ./makeAcornSpreadsheet.sh -td
 echo ""
-echo "---"
-echo ""
+echo "----------------------------------------"
 echo "==> time ./makeAcornSpreadsheet.sh -tld"
 date
 time ./makeAcornSpreadsheet.sh -tld
+echo ""
+echo "========================================"
