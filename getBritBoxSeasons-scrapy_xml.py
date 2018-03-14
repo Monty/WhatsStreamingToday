e6ffbc444cdd21f1b6481ba45f6518debeb2b0f1
Monty Williams <monty.williams@gmail.com>
2018-03-14 14:55:54 -0700

    restore formatting of start_urls

--- getBritBoxSeasons-scrapy_xml.py
+++ getBritBoxSeasons-scrapy_xml.py
@@ -5,19 +5,21 @@ class BritBoxSpider(scrapy.Spider):
     name = "shows"
     #  Doesn't handle /us/programme, or /us/show/ with only one season
     #  Works for /us/show/ with > 1 seson
+    # yapf: disable
     start_urls = [
-        'https://www.britbox.com/us/programmes',  # Top level
-        'https://www.britbox.com/us/movie/70_Glorious_Years_13550',  # Movie
-        'https://www.britbox.com/us/movie/A_Christmas_Carol_15128',  # Movie
-        'https://www.britbox.com/us/show/New_Blood_9275',  # Show (1 season)
-        'https://www.britbox.com/us/show/Till_Death_Us_Do_Part_15810',  # Show (1 season)
+        'https://www.britbox.com/us/programmes',                              # Top level
+        'https://www.britbox.com/us/movie/70_Glorious_Years_13550',           # Movie
+        'https://www.britbox.com/us/movie/A_Christmas_Carol_15128',           # Movie
+        'https://www.britbox.com/us/show/New_Blood_9275',                     # Show (1 season)
+        'https://www.britbox.com/us/show/Till_Death_Us_Do_Part_15810',        # Show (1 season)
         'https://www.britbox.com/us/show/A_History_of_Ancient_Britain_6385',  # Show (1+ seasons)
-        'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',  # Show (+1 seasons)
-        'https://www.britbox.com/us/show/The_Jury_9565',  # Show (2 seasons)
-        'https://www.britbox.com/us/season/Vera_S8_15720',  # Season (2 seasons)
-        'https://www.britbox.com/us/episode/Superhomes_S1_E1_9008',  # Episode (1 season)
-        'https://www.britbox.com/us/episode/Cold_Blood_S2_E2_10298',  # Episode (2 seasons)
+        'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',       # Show (+1 seasons)
+        'https://www.britbox.com/us/show/The_Jury_9565',                      # Show (2 seasons)
+        'https://www.britbox.com/us/season/Vera_S8_15720',                    # Season (2 seasons)
+        'https://www.britbox.com/us/episode/Superhomes_S1_E1_9008',           # Episode (1 season)
+        'https://www.britbox.com/us/episode/Cold_Blood_S2_E2_10298',          # Episode (2 seasons)
     ]
+    # yapf: enable
 
     def parse(self, response):
         for page in self.start_urls:
