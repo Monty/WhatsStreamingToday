dc518fbcbaab1ff3e297a978a98d2dad2c63475a
Monty Williams <monty.williams@gmail.com>
2018-03-13 19:44:53 -0700

    Start splitting out movie and single episode
    Needs improving, especially refacotring

--- getBritBoxSeasons-scrapy_xml.py
+++ getBritBoxSeasons-scrapy_xml.py
@@ -3,10 +3,8 @@ import scrapy
 
 class BritBoxSpider(scrapy.Spider):
     name = "shows"
-    # Doesn't handle /us/programme, /us/movie/, or /us/show/ with only one season
-    # Works for /us/show/ or /us/episode/ with > 1 seson
-    # Don't pass a season, pass the show contaning the season or an episode. Otherwise it 
-    #   gets all seasons, but only the episodes from the first season
+  # Doesn't handle /us/programme, or /us/show/ with only one season
+  # Works for /us/show/ with > 1 seson
     start_urls = [
             'https://www.britbox.com/us/programmes',                             # Top level
             'https://www.britbox.com/us/movie/70_Glorious_Years_13550',          # Movie
@@ -14,19 +12,40 @@ class BritBoxSpider(scrapy.Spider):
             'https://www.britbox.com/us/show/New_Blood_9275',                    # Show (1 season)
             'https://www.britbox.com/us/show/Till_Death_Us_Do_Part_15810',       # Show (1 season)
             'https://www.britbox.com/us/show/A_History_of_Ancient_Britain_6385', # Show (1+ seasons)
-            'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',      # Show (1+ seasons)
+            'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',      # Show (+1 seasons)
             'https://www.britbox.com/us/show/The_Jury_9565',                     # Show (2 seasons)
             'https://www.britbox.com/us/season/Vera_S8_15720',                   # Season (2 seasons)
             'https://www.britbox.com/us/episode/Superhomes_S1_E1_9008',          # Episode (1 season)
             'https://www.britbox.com/us/episode/Cold_Blood_S2_E2_10298',         # Episode (2 seasons)
             ]
 
+
     def parse(self, response):
         for page in self.start_urls:
-            if "/movie/" in page:
+            if "/episode/" in page:
+                yield scrapy.Request(page, callback=self.parse1Episode)
+            elif "/movie/" in page:
                 yield scrapy.Request(page, callback=self.parseEpisode)
             else:
-                yield scrapy.Request(page, callback=self.parse2Seasons)
+                seasons = response.css('div.program-item__title-wrapper p.program-item__program-subtitle::text').extract_first()
+                if seasons is not None:
+                    if "Season " in seasons:
+                        print (seasons)
+                        yield scrapy.Request(page, callback=self.parse2Seasons)
+
+
+    def parseEpisode(self, response):
+        for show in response.css('div.program-item'):
+            yield {
+                    'URL': show.css('a.program-item__block::attr(href)').extract(),
+                    'Title': show.css('h3.program-item__program-title::text').extract(),
+                    'Subtitle': show.css('p.program-item__program-subtitle::text').extract(),
+                    'Duration': show.css('span.programme-metadata__duration::text').re(r'(\d+)'),
+                    'Year': show.css('span.programme-metadata__year::text').extract(),
+                    'Rating': show.css('span.programme-metadata__classification::text').extract(),
+                    'Description': show.css('p.program-item__program-description::text').extract(),
+                    }
+
 
     def parse2Seasons(self, response):
         # for season in response.css('a.brand-season-item'):
@@ -45,14 +64,23 @@ class BritBoxSpider(scrapy.Spider):
                 yield response.follow(href, self.parseEpisode)
 
 
-    def parseEpisode(self, response):
-        for show in response.css('div.program-item'):
-            yield {
-                    'URL': show.css('a.program-item__block::attr(href)').extract(),
-                    'Title': show.css('h3.program-item__program-title::text').extract(),
-                    'Subtitle': show.css('p.program-item__program-subtitle::text').extract(),
-                    'Duration': show.css('span.programme-metadata__duration::text').re(r'(\d+)'),
-                    'Year': show.css('span.programme-metadata__year::text').extract(),
-                    'Rating': show.css('span.programme-metadata__classification::text').extract(),
-                    'Description': show.css('p.program-item__program-description::text').extract(),
-                    }
+    def parse1Episode(self, response):
+        shortURL = "/" + response.url.split('/',3)[3]
+        URLs = (response.css('div.program-item a.program-item__block::attr(href)').extract())
+        for i, item in enumerate(URLs):
+            if (item == shortURL):
+                URL = URLs[i]
+                Title = response.css('div.program-item h3.program-item__program-title::text')[i].extract()
+                Subtitle = response.css('div.program-item p.program-item__program-subtitle::text')[i].extract()
+                Duration = response.css('div.program-item span.programme-metadata__duration::text')[i].re(r'(\d+)')
+                Year = response.css('div.program-item span.programme-metadata__year::text')[i].extract()
+                Rating = response.css('div.program-item span.programme-metadata__classification::text')[i].extract()
+                yield {
+                        'URL': URL,
+                        'Title': Title,
+                        'Subtitle': Subtitle,
+                        'Duration': Duration,
+                        'Year': Year,
+                        'Rating': Rating,
+                        # 'Description': response.css('div.program-item p.program-item__program-description::text')[i].extract(),
+                        }
