54416dc9d2845592b727235a47744c4d1005a13d
Monty Williams <monty.williams@gmail.com>
2018-03-14 08:59:32 -0700

    Improve style

--- getBritBoxSeasons-scrapy_xml.py
+++ getBritBoxSeasons-scrapy_xml.py
@@ -3,22 +3,21 @@ import scrapy
 
 class BritBoxSpider(scrapy.Spider):
     name = "shows"
-  # Doesn't handle /us/programme, or /us/show/ with only one season
-  # Works for /us/show/ with > 1 seson
+    #  Doesn't handle /us/programme, or /us/show/ with only one season
+    #  Works for /us/show/ with > 1 seson
     start_urls = [
-            'https://www.britbox.com/us/programmes',                             # Top level
-            'https://www.britbox.com/us/movie/70_Glorious_Years_13550',          # Movie
-            'https://www.britbox.com/us/movie/A_Christmas_Carol_15128',          # Movie
-            'https://www.britbox.com/us/show/New_Blood_9275',                    # Show (1 season)
-            'https://www.britbox.com/us/show/Till_Death_Us_Do_Part_15810',       # Show (1 season)
-            'https://www.britbox.com/us/show/A_History_of_Ancient_Britain_6385', # Show (1+ seasons)
-            'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',      # Show (+1 seasons)
-            'https://www.britbox.com/us/show/The_Jury_9565',                     # Show (2 seasons)
-            'https://www.britbox.com/us/season/Vera_S8_15720',                   # Season (2 seasons)
-            'https://www.britbox.com/us/episode/Superhomes_S1_E1_9008',          # Episode (1 season)
-            'https://www.britbox.com/us/episode/Cold_Blood_S2_E2_10298',         # Episode (2 seasons)
+            'https://www.britbox.com/us/programmes',                              # Top level
+            'https://www.britbox.com/us/movie/70_Glorious_Years_13550',           # Movie
+            'https://www.britbox.com/us/movie/A_Christmas_Carol_15128',           # Movie
+            'https://www.britbox.com/us/show/New_Blood_9275',                     # Show (1 season)
+            'https://www.britbox.com/us/show/Till_Death_Us_Do_Part_15810',        # Show (1 season)
+            'https://www.britbox.com/us/show/A_History_of_Ancient_Britain_6385',  # Show (1+ seasons)
+            'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',       # Show (+1 seasons)
+            'https://www.britbox.com/us/show/The_Jury_9565',                      # Show (2 seasons)
+            'https://www.britbox.com/us/season/Vera_S8_15720',                    # Season (2 seasons)
+            'https://www.britbox.com/us/episode/Superhomes_S1_E1_9008',           # Episode (1 season)
+            'https://www.britbox.com/us/episode/Cold_Blood_S2_E2_10298',          # Episode (2 seasons)
             ]
-
 
     def parse(self, response):
         for page in self.start_urls:
@@ -30,9 +29,8 @@ class BritBoxSpider(scrapy.Spider):
                 seasons = response.css('div.program-item__title-wrapper p.program-item__program-subtitle::text').extract_first()
                 if seasons is not None:
                     if "Season " in seasons:
-                        print (seasons)
+                        print(seasons)
                         yield scrapy.Request(page, callback=self.parse2Seasons)
-
 
     def parseEpisode(self, response):
         for show in response.css('div.program-item'):
@@ -45,7 +43,6 @@ class BritBoxSpider(scrapy.Spider):
                     'Rating': show.css('span.programme-metadata__classification::text').extract(),
                     'Description': show.css('p.program-item__program-description::text').extract(),
                     }
-
 
     def parse2Seasons(self, response):
         # for season in response.css('a.brand-season-item'):
@@ -63,9 +60,8 @@ class BritBoxSpider(scrapy.Spider):
             for href in response.css('a.brand-season-item::attr(href)'):
                 yield response.follow(href, self.parseEpisode)
 
-
     def parse1Episode(self, response):
-        shortURL = "/" + response.url.split('/',3)[3]
+        shortURL = "/" + response.url.split('/', 3)[3]
         URLs = (response.css('div.program-item a.program-item__block::attr(href)').extract())
         for i, item in enumerate(URLs):
             if (item == shortURL):
