f25236aaa55d62c3f49fa7c6b5a592ca15d77a3a
Monty Williams <monty.williams@gmail.com>
2018-03-14 13:43:54 -0700

    Formatted with yapf

--- getBritBoxSeasons-scrapy_xml.py
+++ getBritBoxSeasons-scrapy_xml.py
@@ -6,18 +6,18 @@ class BritBoxSpider(scrapy.Spider):
     #  Doesn't handle /us/programme, or /us/show/ with only one season
     #  Works for /us/show/ with > 1 seson
     start_urls = [
-            'https://www.britbox.com/us/programmes',                              # Top level
-            'https://www.britbox.com/us/movie/70_Glorious_Years_13550',           # Movie
-            'https://www.britbox.com/us/movie/A_Christmas_Carol_15128',           # Movie
-            'https://www.britbox.com/us/show/New_Blood_9275',                     # Show (1 season)
-            'https://www.britbox.com/us/show/Till_Death_Us_Do_Part_15810',        # Show (1 season)
-            'https://www.britbox.com/us/show/A_History_of_Ancient_Britain_6385',  # Show (1+ seasons)
-            'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',       # Show (+1 seasons)
-            'https://www.britbox.com/us/show/The_Jury_9565',                      # Show (2 seasons)
-            'https://www.britbox.com/us/season/Vera_S8_15720',                    # Season (2 seasons)
-            'https://www.britbox.com/us/episode/Superhomes_S1_E1_9008',           # Episode (1 season)
-            'https://www.britbox.com/us/episode/Cold_Blood_S2_E2_10298',          # Episode (2 seasons)
-            ]
+        'https://www.britbox.com/us/programmes',  # Top level
+        'https://www.britbox.com/us/movie/70_Glorious_Years_13550',  # Movie
+        'https://www.britbox.com/us/movie/A_Christmas_Carol_15128',  # Movie
+        'https://www.britbox.com/us/show/New_Blood_9275',  # Show (1 season)
+        'https://www.britbox.com/us/show/Till_Death_Us_Do_Part_15810',  # Show (1 season)
+        'https://www.britbox.com/us/show/A_History_of_Ancient_Britain_6385',  # Show (1+ seasons)
+        'https://www.britbox.com/us/show/A_Bit_of_Fry_and_Laurie_6393',  # Show (+1 seasons)
+        'https://www.britbox.com/us/show/The_Jury_9565',  # Show (2 seasons)
+        'https://www.britbox.com/us/season/Vera_S8_15720',  # Season (2 seasons)
+        'https://www.britbox.com/us/episode/Superhomes_S1_E1_9008',  # Episode (1 season)
+        'https://www.britbox.com/us/episode/Cold_Blood_S2_E2_10298',  # Episode (2 seasons)
+    ]
 
     def parse(self, response):
         for page in self.start_urls:
@@ -26,7 +26,9 @@ class BritBoxSpider(scrapy.Spider):
             elif "/movie/" in page:
                 yield scrapy.Request(page, callback=self.parseEpisode)
             else:
-                seasons = response.css('div.program-item__title-wrapper p.program-item__program-subtitle::text').extract_first()
+                seasons = response.css(
+                    'div.program-item__title-wrapper p.program-item__program-subtitle::text'
+                ).extract_first()
                 if seasons is not None:
                     if "Season " in seasons:
                         print(seasons)
@@ -35,14 +37,14 @@ class BritBoxSpider(scrapy.Spider):
     def parseEpisode(self, response):
         for show in response.css('div.program-item'):
             yield {
-                    'URL': show.css('a.program-item__block::attr(href)').extract(),
-                    'Title': show.css('h3.program-item__program-title::text').extract(),
-                    'Subtitle': show.css('p.program-item__program-subtitle::text').extract(),
-                    'Duration': show.css('span.programme-metadata__duration::text').re(r'(\d+)'),
-                    'Year': show.css('span.programme-metadata__year::text').extract(),
-                    'Rating': show.css('span.programme-metadata__classification::text').extract(),
-                    'Description': show.css('p.program-item__program-description::text').extract(),
-                    }
+                'URL': show.css('a.program-item__block::attr(href)').extract(),
+                'Title': show.css('h3.program-item__program-title::text').extract(),
+                'Subtitle': show.css('p.program-item__program-subtitle::text').extract(),
+                'Duration': show.css('span.programme-metadata__duration::text').re(r'(\d+)'),
+                'Year': show.css('span.programme-metadata__year::text').extract(),
+                'Rating': show.css('span.programme-metadata__classification::text').extract(),
+                'Description': show.css('p.program-item__program-description::text').extract(),
+            }
 
     def parse2Seasons(self, response):
         # for season in response.css('a.brand-season-item'):
@@ -50,12 +52,12 @@ class BritBoxSpider(scrapy.Spider):
 
         for season in response.css('a.brand-season-item'):
             yield {
-                    'URL': season.css('a.brand-season-item::attr(href)').extract(),
-                    'Title': title,
-                    'SeasonNumber': season.css('h2.program-item__program-title::text').re(r'Season (\d+)'),
-                    'Year': season.css('p.season-metadata::text').extract(),
-                    'NumEpisodes': season.css('p.season-metadata span::text')[2].re(r'(\d+)'),
-                    }
+                'URL': season.css('a.brand-season-item::attr(href)').extract(),
+                'Title': title,
+                'SeasonNumber': season.css('h2.program-item__program-title::text').re(r'Season (\d+)'),
+                'Year': season.css('p.season-metadata::text').extract(),
+                'NumEpisodes': season.css('p.season-metadata span::text')[2].re(r'(\d+)'),
+            }
 
             for href in response.css('a.brand-season-item::attr(href)'):
                 yield response.follow(href, self.parseEpisode)
@@ -66,17 +68,22 @@ class BritBoxSpider(scrapy.Spider):
         for i, item in enumerate(URLs):
             if (item == shortURL):
                 URL = URLs[i]
-                Title = response.css('div.program-item h3.program-item__program-title::text')[i].extract()
-                Subtitle = response.css('div.program-item p.program-item__program-subtitle::text')[i].extract()
-                Duration = response.css('div.program-item span.programme-metadata__duration::text')[i].re(r'(\d+)')
-                Year = response.css('div.program-item span.programme-metadata__year::text')[i].extract()
-                Rating = response.css('div.program-item span.programme-metadata__classification::text')[i].extract()
+                Title = response.css('div.program-item h3.program-item__program-title::text')[
+                    i].extract()
+                Subtitle = response.css('div.program-item p.program-item__program-subtitle::text')[
+                    i].extract()
+                Duration = response.css('div.program-item span.programme-metadata__duration::text')[
+                    i].re(r'(\d+)')
+                Year = response.css('div.program-item span.programme-metadata__year::text')[
+                    i].extract()
+                Rating = response.css(
+                    'div.program-item span.programme-metadata__classification::text')[i].extract()
                 yield {
-                        'URL': URL,
-                        'Title': Title,
-                        'Subtitle': Subtitle,
-                        'Duration': Duration,
-                        'Year': Year,
-                        'Rating': Rating,
-                        # 'Description': response.css('div.program-item p.program-item__program-description::text')[i].extract(),
-                        }
+                    'URL': URL,
+                    'Title': Title,
+                    'Subtitle': Subtitle,
+                    'Duration': Duration,
+                    'Year': Year,
+                    'Rating': Rating,
+                    # 'Description': response.css('div.program-item p.program-item__program-description::text')[i].extract(),
+                }
