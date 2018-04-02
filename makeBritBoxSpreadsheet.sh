9b2b216c7430d941e5e7314245f07580938ded94
Monty Williams <monty.williams@gmail.com>
2018-04-01 20:36:28 -0700

    Get rid of unnecessary files carried over from non-BritBox processing

--- makeBritBoxSpreadsheet.sh
+++ makeBritBoxSpreadsheet.sh
@@ -51,33 +51,19 @@ mkdir -p $COLUMNS $BASELINE
 # so if you change them here, change them there as well
 # they are named with today's date so running them twice
 # in one day will only generate one set of results
-URL_FILE="$COLUMNS/urls-$DATE.csv"
-PUBLISHED_URLS="$BASELINE/urls.txt"
 PROGRAMS_SPREADSHEET_FILE="$COLUMNS/BritBoxPrograms-$DATE.csv"
 SEASONS_SPREADSHEET_FILE="$COLUMNS/BritBoxSeasons-$DATE.csv"
 EPISODES_SPREADSHEET_FILE="$COLUMNS/BritBoxEpisodes-$DATE.csv"
 PROGRAMS_FILE="$COLUMNS/BritBoxPrograms.csv"
 SEASONS_FILE="$COLUMNS/BritBoxSeasons.csv"
 EPISODES_FILE="$COLUMNS/BritBoxEpisodes.csv"
-MARQUEE_FILE="$COLUMNS/marquees-$DATE.csv"
-PUBLISHED_MARQUEES="$BASELINE/marquees.txt"
-TITLE_FILE="$COLUMNS/titles-$DATE.csv"
-PUBLISHED_TITLES="$BASELINE/titles.txt"
-LINK_FILE="$COLUMNS/links-$DATE.csv"
-PUBLISHED_LINKS="$BASELINE/links.txt"
+# Probably obsolete
 DESCRIPTION_FILE="$COLUMNS/descriptions-$DATE.csv"
 PUBLISHED_DESCRIPTIONS="$BASELINE/descriptions.txt"
-NUM_SEASONS_FILE="$COLUMNS/numberOfSeasons-$DATE.csv"
-PUBLISHED_NUM_SEASONS="$BASELINE/numberOfSeasons.txt"
-NUM_EPISODES_FILE="$COLUMNS/numberOfEpisodes-$DATE.csv"
-PUBLISHED_NUM_EPISODES="$BASELINE/numberOfEpisodes.txt"
 DURATION_FILE="$COLUMNS/durations-$DATE.csv"
 PUBLISHED_DURATIONS="$BASELINE/durations.txt"
 HEADER_FILE="$COLUMNS/headers-$DATE.csv"
 PUBLISHED_HEADERS="$BASELINE/headers.txt"
-#
-EPISODE_URL_FILE="$COLUMNS/episodeUrls-$DATE.csv"
-PUBLISHED_EPISODE_URLS="$BASELINE/episodeUrls.txt"
 EPISODE_INFO_FILE="$COLUMNS/episodeInfo-$DATE.csv"
 PUBLISHED_EPISODE_INFO="$BASELINE/episodeInfo.txt"
 #
@@ -93,9 +79,8 @@ fi
 POSSIBLE_DIFFS="BritBox_diffs-$LONGDATE.txt"
 ERROR_FILE="BritBox_stderr-$LONGDATE.txt"
 
-rm -f $URL_FILE $MARQUEE_FILE $TITLE_FILE $LINK_FILE $DESCRIPTION_FILE $NUM_SEASONS_FILE \
-    $NUM_EPISODES_FILE $DURATION_FILE $HEADER_FILE $SPREADSHEET_FILE $EPISODE_URL_FILE \
-    $EPISODE_INFO_FILE $PROGRAMS_SPREADSHEET_FILE $SEASONS_SPREADSHEET_FILE $EPISODES_SPREADSHEET_FILE
+rm -f $DESCRIPTION_FILE $DURATION_FILE $HEADER_FILE $EPISODE_INFO_FILE \
+    $SPREADSHEET_FILE $PROGRAMS_SPREADSHEET_FILE $SEASONS_SPREADSHEET_FILE $EPISODES_SPREADSHEET_FILE
 
 # Generate spreadsheets from BritBox "Programmes A-Z" page
 csvformat -T $PROGRAMS_FILE | grep "^1" | sort -df --field-separator=$'\t' --key=4,4 |
@@ -109,52 +94,6 @@ exit
 
 # Print header for possible errors from processing series
 printf "### Possible anomalies from processing series are listed below.\n\n" >$ERROR_FILE
-
-# keep track of the number of rows in the spreadsheet
-lastRow=1
-# loop through the list of series URLs from $URL_FILE
-while read -r line; do
-    # Generate series Marquees, Descriptions, Headers from BritBox series pages
-    #     (Headers include Genre, Country, Language, Rating)
-    # and the list of Episode URLs for further processing.
-    curl -sS "$line" |
-        awk -v MARQUEE_FILE=$MARQUEE_FILE -v DESCRIPTION_FILE=$DESCRIPTION_FILE \
-            -v HEADER_FILE=$HEADER_FILE -v EPISODE_URL_FILE=$EPISODE_URL_FILE \
-            -v ERROR_FILE=$ERROR_FILE -f getBritBoxFrom-seriesPages.awk
-    ((lastRow++))
-done <"$URL_FILE"
-
-# keep track of the series number we're processing
-currentSeriesNumber=0
-# loop through the list of episode URLs from $EPISODE_URL_FILE
-while read -r episode_URL; do
-    # Control the context of the NUM_EPISODES_FILE when a new series is found
-    # Special handling for Don Matteo which starts at season 4
-    if [[ ${episode_URL} =~ season:1 || ${episode_URL} =~ "don-matteo/season:4" ]]; then
-        ((currentSeriesNumber++))
-        if [[ -e $NUM_EPISODES_FILE ]]; then
-            echo >>$NUM_EPISODES_FILE
-        fi
-        echo -n "=" >>$NUM_EPISODES_FILE
-    fi
-    # Generate a separate file with a line for each episode containing
-    # seriesNumber, episodeURL, seriesTitle, seasonNumber, episodeNumber,
-    # episodeTitle, episodeDuration, & episodeDescription with the same columns
-    # as the primary spreadsheet so they can be combined into one.
-    # Add to the number of episodes in a series but with no terminating newline
-    curl -sS "$episode_URL" |
-        awk -v EPISODE_INFO_FILE=$EPISODE_INFO_FILE -v SERIES_NUMBER=$currentSeriesNumber \
-            -v NUM_EPISODES_FILE=$NUM_EPISODES_FILE -v ERROR_FILE=$ERROR_FILE \
-            -f getBritBoxFrom-episodePages.awk
-done <"$EPISODE_URL_FILE"
-# Add final newline
-echo >>$NUM_EPISODES_FILE
-
-# Join the URL and Title into a hyperlink
-# WARNING there is an actual tab character in the following command
-# because sed in macOS doesn't regognize \t
-paste $URL_FILE $TITLE_FILE |
-    sed -e 's/^/=HYPERLINK("/; s/	/"\;"/; s/$/")/' >>$LINK_FILE
 
 # Total the duration of all episodes in every series
 totalTime=$(awk -v DURATION_FILE=$DURATION_FILE -f calculateDurations.awk $EPISODE_INFO_FILE)
@@ -186,11 +125,11 @@ if [ "$UNSORTED" = "yes" ]; then
     # sort key 1 sorts in the order found on the web
     # sort key 4 sorts by title
     # both sort $file2 by season then episode (if one is provided)
-    paste $LINK_FILE $NUM_SEASONS_FILE $NUM_EPISODES_FILE $DURATION_FILE $HEADER_FILE \
+    paste $DURATION_FILE $HEADER_FILE \
         $DESCRIPTION_FILE | awk '{print NR"\t"$0}' | cat - $file2 |
         sort --key=1,1n --key=4 --field-separator=\" >>$SPREADSHEET_FILE
 else
-    paste $LINK_FILE $NUM_SEASONS_FILE $NUM_EPISODES_FILE $DURATION_FILE $HEADER_FILE \
+    paste $DURATION_FILE $HEADER_FILE \
         $DESCRIPTION_FILE | awk '{print NR"\t"$0}' | cat - $file2 |
         sort --key=4 --field-separator=\" >>$SPREADSHEET_FILE
 fi
@@ -246,24 +185,12 @@ function checkdiffs() {
 cat >>$POSSIBLE_DIFFS <<EOF
 ==> ${0##*/} completed: $(date)
 
-$(checkdiffs $MARQUEE_FILE $TITLE_FILE)
-
 
 ### MHz shows on the web are not sorted by title. When shows are
 ### rearranged, the individual column diffs may look significant.
 ### Check the "titles" column diffs to see if the changes are only
 ### from identical titles being deleted and re-inserted.
-$(checkdiffs $PUBLISHED_TITLES $TITLE_FILE)
-$(checkdiffs $PUBLISHED_MARQUEES $MARQUEE_FILE)
-$(checkdiffs $PUBLISHED_URLS $URL_FILE)
-$(checkdiffs $PUBLISHED_LINKS $LINK_FILE)
-$(checkdiffs $PUBLISHED_DESCRIPTIONS $DESCRIPTION_FILE)
-$(checkdiffs $PUBLISHED_HEADERS $HEADER_FILE)
-$(checkdiffs $PUBLISHED_NUM_SEASONS $NUM_SEASONS_FILE)
-$(checkdiffs $PUBLISHED_NUM_EPISODES $NUM_EPISODES_FILE)
 $(checkdiffs $PUBLISHED_DURATIONS $DURATION_FILE)
-$(checkdiffs $PUBLISHED_EPISODE_URLS $EPISODE_URL_FILE)
-$(checkdiffs $PUBLISHED_EPISODE_INFO $EPISODE_INFO_FILE)
 
 
 ### Any funny stuff with file lengths? Any differences in
