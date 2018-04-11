e38a32f28efbbd271874e7b03a43751af131e260
Monty Williams <monty.williams@gmail.com>
2018-04-11 15:54:03 -0700

    Start switching strategy to multiple startUrls

---
+++ buildBritBoxScrapersFrom-webscraper.awk
@@ -0,0 +1,24 @@
+BEGIN {
+    FS = "\""
+}
+
+/\/us\/movie\// {
+    numMovies += 1
+    shortURL = $6
+    printf ("%s    \"https://www.britbox.com%s\"", trailingMovieComma, shortURL) >> MOVIES_JSON_FILE
+    trailingMovieComma = ",\n"
+}
+
+/\/us\/show\// {
+    numShows += 1
+    shortURL = $6
+    printf ("%s    \"https://www.britbox.com%s\"", trailingShowComma, shortURL) >> SHOWS_JSON_FILE
+    trailingShowComma = ",\n"
+}
+
+END {
+    print "" >> MOVIES_JSON_FILE
+    print "" >> SHOWS_JSON_FILE
+    print "==> Found " numMovies " movies"
+    print "==> Found " numShows " shows"
+}
---
+++ buildBritBoxScrapersFrom-webscraper.sh
@@ -0,0 +1,24 @@
+#! /bin/bash
+# Use the URLs output by getBritBoxProgramsFrom-webscraper.json to generate json scrapers
+# with a per show startUrl instead of the top level "https://www.britbox.com/us/programmes"
+#
+# Then use the resulting json files to scrape current movies and shows 
+
+DATE="$(date +%y%m%d)"
+LONGDATE="$(date +%y%m%d.%H%M%S)"
+
+COLUMNS="BritBox-columns"
+SCRAPES="BritBox-scrapes"
+
+PROGRAMS_FILE="$SCRAPES/BritBoxPrograms.csv"
+MOVIES_JSON_FILE="$COLUMNS/BritBoxMovies-$LONGDATE.json"
+SHOWS_JSON_FILE="$COLUMNS/BritBoxShows-$LONGDATE.json"
+
+grep -B4 startUrl movieTemplate.json > $MOVIES_JSON_FILE
+grep -B4 startUrl showTemplate.json > $SHOWS_JSON_FILE
+
+awk -v MOVIES_JSON_FILE=$MOVIES_JSON_FILE -v SHOWS_JSON_FILE=$SHOWS_JSON_FILE \
+    -f buildBritBoxScrapersFrom-webscraper.awk $PROGRAMS_FILE
+
+grep -B1 -A99 selectors movieTemplate.json >> $MOVIES_JSON_FILE
+grep -B1 -A99 selectors showTemplate.json >> $SHOWS_JSON_FILE
