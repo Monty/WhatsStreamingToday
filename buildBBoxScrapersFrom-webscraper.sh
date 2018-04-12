d96a1f980ed102eabf1c7aca8210e675786ea525
Monty Williams <monty.williams@gmail.com>
2018-04-12 16:23:20 -0700

    Corrected seasons template name

--- buildBBoxScrapersFrom-webscraper.sh
+++ buildBBoxScrapersFrom-webscraper.sh
@@ -21,11 +21,11 @@ SEASONS_JSON_FILE="$SEASONS_ID.json"
 
 grep -B4 startUrl movieTemplate.json | sed -e "s/BBoxMovies/$MOVIES_ID/" >$MOVIES_JSON_FILE
 grep -B4 startUrl episodeTemplate.json | sed -e "s/BBoxEpisodes/$EPISODES_ID/" >$EPISODES_JSON_FILE
-grep -B4 startUrl episodeTemplate.json | sed -e "s/BBoxSeasons/$SEASONS_ID/" >$SEASONS_JSON_FILE
+grep -B4 startUrl seasonTemplate.json | sed -e "s/BBoxSeasons/$SEASONS_ID/" >$SEASONS_JSON_FILE
 
 awk -v MOVIES_JSON_FILE=$MOVIES_JSON_FILE -v EPISODES_JSON_FILE=$EPISODES_JSON_FILE \
     -v SEASONS_JSON_FILE=$SEASONS_JSON_FILE -f buildBBoxScrapersFrom-webscraper.awk $PROGRAMS_FILE
 
 grep -B1 -A99 selectors movieTemplate.json >>$MOVIES_JSON_FILE
 grep -B1 -A99 selectors episodeTemplate.json >>$EPISODES_JSON_FILE
-grep -B1 -A99 selectors episodeTemplate.json >>$SEASONS_JSON_FILE
+grep -B1 -A99 selectors seasonTemplate.json >>$SEASONS_JSON_FILE
