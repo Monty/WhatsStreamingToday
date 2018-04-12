8f37341c7497c95fec044eddd999d9fa9c9b401a
Monty Williams <monty.williams@gmail.com>
2018-04-12 15:41:57 -0700

    Add building seasons scraper

--- buildBBoxScrapersFrom-webscraper.sh
+++ buildBBoxScrapersFrom-webscraper.sh
@@ -10,16 +10,22 @@ LONGDATE="$(date +%y%m%d.%H%M%S)"
 SCRAPES="BritBox-scrapes"
 
 PROGRAMS_FILE="$SCRAPES/BritBoxPrograms.csv"
+#
 MOVIES_ID="BBoxMovies-$LONGDATE"
 EPISODES_ID="BBoxEpisodes-$LONGDATE"
+SEASONS_ID="BBoxSeasons-$LONGDATE"
+#
 MOVIES_JSON_FILE="$MOVIES_ID.json"
 EPISODES_JSON_FILE="$EPISODES_ID.json"
+SEASONS_JSON_FILE="$SEASONS_ID.json"
 
 grep -B4 startUrl movieTemplate.json | sed -e "s/BBoxMovies/$MOVIES_ID/" >$MOVIES_JSON_FILE
 grep -B4 startUrl episodeTemplate.json | sed -e "s/BBoxEpisodes/$EPISODES_ID/" >$EPISODES_JSON_FILE
+grep -B4 startUrl episodeTemplate.json | sed -e "s/BBoxSeasons/$SEASONS_ID/" >$SEASONS_JSON_FILE
 
 awk -v MOVIES_JSON_FILE=$MOVIES_JSON_FILE -v EPISODES_JSON_FILE=$EPISODES_JSON_FILE \
-    -f buildBBoxScrapersFrom-webscraper.awk $PROGRAMS_FILE
+    -v SEASONS_JSON_FILE=$SEASONS_JSON_FILE -f buildBBoxScrapersFrom-webscraper.awk $PROGRAMS_FILE
 
 grep -B1 -A99 selectors movieTemplate.json >>$MOVIES_JSON_FILE
 grep -B1 -A99 selectors episodeTemplate.json >>$EPISODES_JSON_FILE
+grep -B1 -A99 selectors episodeTemplate.json >>$SEASONS_JSON_FILE
