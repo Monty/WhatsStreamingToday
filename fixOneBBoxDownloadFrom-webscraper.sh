42f0571cc9b5a219abc28464b5f5d6b092849ff9
Monty Williams <monty.williams@gmail.com>
2018-04-18 17:01:50 -0700

    Add script to canonicalize webscraper downloads

---
+++ fixOneBBoxDownloadFrom-webscraper.sh
@@ -0,0 +1,8 @@
+# Create a cannonical form of a webscraper download. Simplifies cutting specific fields.
+# Useful for comparing files. Not useful for many purposes as it only deletes the first field
+# and know nothng about ther useless fields.
+
+# Join broken lines, get rid of useless 'web-scraper-order' field, change comma-separated to
+# tab separated, sort into useful order
+awk -f fixExtraLinesFrom-webscraper.awk $1 | cut -f 2- -d "," | csvformat -T |
+    grep '/us/' | sort -df --field-separator=$'\t' --key=1,1
