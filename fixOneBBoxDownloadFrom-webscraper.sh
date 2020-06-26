667d9a14759bb6823455c70e524b4b624993da31
Monty Williams <monty.williams@gmail.com>
2020-06-26 16:55:34 -0700

    Remove webscraper BBox processing

--- fixOneBBoxDownloadFrom-webscraper.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# Create a canonical form of a webscraper download. Simplifies cutting specific fields.
-# Useful for comparing files. Not useful for many purposes as it only deletes the first field
-# and know nothing about their useless fields.
-
-# Join broken lines, get rid of useless 'web-scraper-order' field, change comma-separated to
-# tab separated, sort into useful order
-awk -f fixExtraLinesFrom-webscraper.awk $1 | cut -f 2- -d "," | csvformat -T |
-    grep '/us/' | sort -df --field-separator=$'\t' --key=1,1
