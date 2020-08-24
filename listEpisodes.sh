e457a3b7c73b04d0a29eccb8c80df16a1c92698d
Monty Williams <monty.williams@gmail.com>
2020-08-23 18:12:14 -0700

    Cleanup duration conversion

--- listEpisodes.sh
+++ listEpisodes.sh
@@ -3,14 +3,14 @@
 # If you get too many results, copy/paste a portion of the title string from one of the results
 #
 # EXAMPLES:
-#    ./listEpisodes.sh wallander               131 lines
-#    ./listEpisodes.sh Wallander,               90 lines
-#    ./listEpisodes.sh Wallander, S03           27 lines
-#    ./listEpisodes.sh Wallander, S03E03         3 lines
+#    ./listEpisodes.sh wallander               184 lines
+#    ./listEpisodes.sh Wallander,              127 lines
+#    ./listEpisodes.sh Wallander, S03           38 lines
+#    ./listEpisodes.sh Wallander, S03E03         4 lines
 #
 # Some regular expressions require quotes
-#    ./listEpisodes.sh Wallander, S0..03         6 lines
-#    ./listEpisodes.sh "Wallander, S0[2-3]"     34 lines
+#    ./listEpisodes.sh Wallander, S0..03         8 lines
+#    ./listEpisodes.sh "Wallander, S0[2-3]"     48 lines
 #
 # Make sure we are in the correct directory
 DIRNAME=$(dirname "$0")
@@ -25,7 +25,8 @@ if [ $ACORN ] && [ $(cut -f 1 $ACORN | grep -i -c "$*") != 0 ]; then
     linesFound="yes"
     grep -i "$*" $ACORN | cut -f 1,4,5 |
         perl -F"\t" -lane 'for ($F[0]) {s/^.*";"//; s/"\)$//;};
-        printf "%-80s%12s\n    %s\n\n", $F[0], $F[1], $F[2]' | fmt -w 92
+            for ($F[1]) {s/0(\d):/$1:/g; s/:/h /; s/:.*/m/; s/^0h //; s/^0m//;};
+            printf "%-80s%12s\n    %s\n\n", $F[0], $F[1], $F[2]' | fmt -w 92
 fi
 
 if [ $BBOX ] && [ $(cut -f 2 $BBOX | grep -i -c "$*") != 0 ]; then
@@ -36,7 +37,8 @@ if [ $BBOX ] && [ $(cut -f 2 $BBOX | grep -i -c "$*") != 0 ]; then
     linesFound="yes"
     grep -i "$*" $BBOX | cut -f 2,5,9 |
         perl -F"\t" -lane 'for ($F[0]) {s/^.*";"//; s/"\)$//;};
-        printf "%-80s%12s\n    %s\n\n", $F[0], $F[1], $F[2]' | fmt -w 92
+            for ($F[1]) {s/0(\d):/$1:/g; s/:/h /; s/:.*/m/; s/^0h //; s/^0m//;};
+            printf "%-80s%12s\n    %s\n\n", $F[0], $F[1], $F[2]' | fmt -w 92
 fi
 
 if [ $MHZ ] && [ $(cut -f 1 $MHZ | grep -i -c "$*") != 0 ]; then
@@ -46,5 +48,6 @@ if [ $MHZ ] && [ $(cut -f 1 $MHZ | grep -i -c "$*") != 0 ]; then
     echo "==> From $MHZ"
     grep -i "$*" $MHZ | cut -f 1,4,9 |
         perl -F"\t" -lane 'for ($F[0]) {s/^.*";"//; s/"\)$//;};
-        printf "%-80s%12s\n    %s\n\n", $F[0], $F[1], $F[2]' | fmt -w 92
+            for ($F[1]) {s/0(\d):/$1:/g; s/:/h /; s/:.*/m/; s/^0h //; s/^0m//;};
+            printf "%-80s%12s\n    %s\n\n", $F[0], $F[1], $F[2]' | fmt -w 92
 fi
