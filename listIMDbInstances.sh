813afe707c8aa3ae38e38f6f3be945897832ce6c
Monty Williams <monty.williams@gmail.com>
2020-10-30 17:14:34 -0700

    Create listIMDbInstances.sh

---
+++ listIMDbInstances.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Print the instances of any "word" in downloaded IMDb data files
+
+# INVOCATION:
+#    listIMDbInstances.sh tt1809792
+#    listIMDbInstances.sh nm4257020
+#    listIMDbInstances.sh Montalbano
+
+for file in title.basics.tsv.gz title.akas.tsv.gz title.principals.tsv.gz title.crew.tsv.gz \
+    title.episode.tsv.gz name.basics.tsv.gz; do
+    if [ -e "$file" ]; then
+        printf "==> $file\n"
+        rg -wNz $1 $file
+        printf "\n"
+    fi
+done
