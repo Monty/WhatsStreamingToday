281bf7839d3e1946b802f85355221110850c0450
Monty Williams <monty.williams@gmail.com>
2023-06-15 15:22:57 -0700

    Grab both all series and all movies

--- Viaplay/index.js
+++ Viaplay/index.js
@@ -4,10 +4,19 @@ const { chromium } = require('playwright');
   const browser = await chromium.launch({
     headless: false,
   });
-  const context = await browser.newContext({ storageState: 'auth/cookies.json' });
+  const context = await browser.newContext({
+    storageState: 'auth/cookies.json',
+  });
   const page = await context.newPage();
+  // Grab data from all series
   await page.goto('https://viaplay.com/us-en/series/all');
-  // await page.getByRole('button', { name: 'Accept all' }).click();
+  await page.getByRole('button', { name: 'Popular' }).click();
+  await page.getByRole('link', { name: 'Last added' }).click();
+  await page.waitForTimeout(15000); // wait for 15 seconds
+  // Grab data from all movies
+  await page.goto('https://viaplay.com/us-en/movies/all');
+  await page.getByRole('button', { name: 'Popular' }).click();
+  await page.getByRole('link', { name: 'Last added' }).click();
   await page.waitForTimeout(15000); // wait for 15 seconds
   await context.storageState({ path: 'auth/cookies.json' });
   // ---------------------
