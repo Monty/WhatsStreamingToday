bf0aa9f06b449de1b54623bff02ebe0450082bbd
Monty Williams <monty.williams@gmail.com>
2023-06-17 14:15:51 -0700

    Grab all page content

--- Viaplay/index.js
+++ Viaplay/index.js
@@ -1,4 +1,5 @@
 const { chromium } = require('playwright');
+let fs = require('fs');
 
 (async () => {
   const browser = await chromium.launch({
@@ -29,6 +30,18 @@ const { chromium } = require('playwright');
   await page.waitForTimeout(2000); // wait for 2 seconds
   await page.mouse.wheel(0, 4000)
   await page.waitForTimeout(5000); // wait for 5 seconds
+  const series = await page.content();
+  fs.writeFile(
+    'allSeries.html',
+    '<!-- Data from https://viaplay.com/us-en/series/all-->\n\n' +
+      series +
+      '\n',
+    (err) => {
+      if (err) throw err;
+      console.log('<== Done writting allSeries.html');
+    }
+  );
+
   // Grab data from all movies
   await page.goto('https://viaplay.com/us-en/movies/all');
   await page.getByRole('button', { name: 'Popular' }).click();
@@ -45,6 +58,16 @@ const { chromium } = require('playwright');
   await page.mouse.wheel(0, 4000)
   await page.waitForTimeout(5000); // wait for 5 seconds
   await context.storageState({ path: 'auth/cookies.json' });
+  const movies = await page.content();
+  fs.writeFile(
+    'allMovies.html',
+    '<!--Data from https://viaplay.com/us-en/movies/all-->\n\n' + movies + '\n',
+    (err) => {
+      if (err) throw err;
+      console.log('<== Done writting allMovies.html');
+    }
+  );
+
   // ---------------------
   await context.close();
   await browser.close();
