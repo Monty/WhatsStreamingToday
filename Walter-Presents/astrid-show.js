6117f397c4ee8537ff4d010dbe2f82762c345edb
Monty Williams <monty.williams@gmail.com>
2023-06-18 16:34:26 -0700

    Add Astrid

---
+++ Walter-Presents/astrid-show.js
@@ -0,0 +1,26 @@
+const { chromium } = require('playwright');
+let fs = require('fs');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+  const context = await browser.newContext({
+    storageState: 'auth/cookies.json',
+  });
+  const page = await context.newPage();
+  await page.goto('https://www.pbs.org/show/astrid/');
+  const show = await page.content();
+  fs.writeFile(
+    'astrid-show.html',
+    '<!-- Data from https://www.pbs.org/show/astrid/ -->\n\n' + show + '\n',
+    (err) => {
+      if (err) throw err;
+      console.log('<== Done writting astrid-show.html');
+    }
+  );
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+})();
