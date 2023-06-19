9082c608f3482bc2810b8fafce8153126a8bf38f
Monty Williams <monty.williams@gmail.com>
2023-06-19 11:03:36 -0700

    Create endeavour-show.js

---
+++ Walter-Presents/endeavour-show.js
@@ -0,0 +1,37 @@
+const { chromium } = require('playwright');
+let fs = require('fs');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: false,
+  });
+  const context = await browser.newContext({
+    storageState: 'auth/cookies.json',
+  });
+  const page = await context.newPage();
+  await page.goto('https://www.pbs.org/show/endeavour/');
+  try {
+    for (let i = 0; i < 12; i++) {
+      await page
+        .locator('#splide01')
+        .getByRole('button', { name: 'Next slide' })
+        .click({ timeout: 1000 });
+      await page.waitForTimeout(1000); // wait for 1 seconds
+    }
+  } catch {
+    console.log('<== Exceeded "Next slide" count.');
+  }
+  const show = await page.content();
+  fs.writeFile(
+    'endeavour-show.html',
+    '<!-- Data from https://www.pbs.org/show/endeavour/ -->\n\n' + show + '\n',
+    (err) => {
+      if (err) throw err;
+      console.log('<== Done writing endeavour-show.html');
+    }
+  );
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+})();
