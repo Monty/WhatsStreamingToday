cdf4ea4f841154a971a48edccd119a2acee5c72a
Monty Williams <monty.williams@gmail.com>
2023-06-19 11:21:12 -0700

    Try 11 "Next slide" clicks
    Make sure all episodes appear in raw_html output.
    
    Quit before 11 clicks if button becomes hidden.
    
    Generate message if 11 clicks isn't enough.

--- Walter-Presents/env-show.js
+++ Walter-Presents/env-show.js
@@ -2,7 +2,7 @@ const { chromium } = require('playwright');
 let fs = require('fs');
 
 let series_name = process.env.TARGET;
-console.log('TARGET = ' + series_name);
+console.log('<== TARGET = ' + series_name);
 
 (async () => {
   const browser = await chromium.launch({
@@ -13,6 +13,16 @@ console.log('TARGET = ' + series_name);
   });
   const page = await context.newPage();
   await page.goto('https://www.pbs.org/show/' + series_name);
+  try {
+    for (let i = 0; i < 11; i++) {
+      await page
+        .locator('#splide01')
+        .getByRole('button', { name: 'Next slide' })
+        .click({ timeout: 1000 });
+      await page.waitForTimeout(1000); // wait for 1 seconds
+    }
+    console.log('<== ' + series_name + ' Not enough "Next slide" clicks!');
+  } catch {}
   const raw_html = await page.content();
   fs.writeFile(
     series_name + '-show.html',
