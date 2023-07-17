90fd5cf68cf65ead9a0c70e3b15eae554b051afe
Monty Williams <monty.williams@gmail.com>
2023-07-16 18:24:31 -0700

    Generic episode processor

---
+++ Walter-Presents/env-episode.js
@@ -0,0 +1,31 @@
+const { chromium } = require('playwright');
+let fs = require('fs');
+
+let episode_URL = process.env.TARGET;
+console.log('==> TARGET = ' + episode_URL);
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: true,
+  });
+  const context = await browser.newContext({
+    storageState: 'auth/cookies.json',
+  });
+  const page = await context.newPage();
+  await page.goto(episode_URL);
+  const description = await page
+    .locator('//*[@id="maincontent"]/div[1]/article/div/div[1]/p[3]')
+    .innerText();
+  fs.appendFile(
+    'env-episode.txt',
+    'URL="' + episode_URL + '"\n' + 'Description="' + description + '"\n',
+    (err) => {
+      if (err) throw err;
+      console.log('==> Done writing env-episode.txt');
+    }
+  );
+
+  // ---------------------
+  await context.close();
+  await browser.close();
+})();
