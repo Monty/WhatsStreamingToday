901b194ed46e91d369ede9f78b44a6b008649962
Monty Williams <monty.williams@gmail.com>
2023-07-17 17:36:40 -0700

    Remove 'Rating: ' from rating variable

--- Walter-Presents/env-episode.js
+++ Walter-Presents/env-episode.js
@@ -2,7 +2,7 @@ const { chromium } = require('playwright');
 let fs = require('fs');
 
 let episode_ID = process.env.TARGET;
-let episode_URL = "https://www.pbs.org/video" + episode_ID;
+let episode_URL = 'https://www.pbs.org/video' + episode_ID;
 let output_file = process.env.AWK_EPISODES;
 console.log('\n==> Processing ' + episode_ID);
 
@@ -18,12 +18,13 @@ console.log('\n==> Processing ' + episode_ID);
   const description = await page
     .locator('//*[@id="maincontent"]/div[1]/article/div/div[1]/p[3]')
     .innerText();
-  const rating = await page
+  let rating = await page
     .locator('//*[@id="maincontent"]/div[1]/article/div/div[1]/div[2]/p[3]')
     .innerText();
+  rating = rating.replace('Rating: ', '');
   fs.appendFile(
     output_file,
-      episode_ID +
+    episode_ID +
       ' { $7="' +
       rating +
       '"; $8="' +
