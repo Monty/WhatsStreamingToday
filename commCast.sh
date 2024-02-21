36bc950bbad2b000544d896c4a611467cb825a90
Monty Williams <monty.williams@gmail.com>
2024-02-20 23:48:47 -0800

    Clean up commCast.sh

--- commCast.sh
+++ commCast.sh
@@ -3,22 +3,28 @@
 # Find common cast members between multiple shows
 #
 # NOTES:
-#   Requires cast member files produced by makeIMDbFromFiles-noHype.sh and/or other make*.sh scripts.
-#   Cast member data from IMDb and streaming service providers often has mistakes.
-#   Titles may differ between services, e.g. Gåsmamman/gasmamman, Jägarna/the-hunters
+#   Requires cast member files produced by make*.sh scripts.
+#
+#   Cast member data from streaming service providers often has mistakes.
+#
+#   Titles may differ between services,
+#       e.g. Gåsmamman/gasmamman, Jägarna/the-hunters
 #
 #   To help refine searches, the output is rather wordy.
+#
 #   The final section (Duplicated names) is the section of interest.
 #
 #   It may help to start with an actor, e.g.
 #       ./commCast.sh 'Alexandra Rapaport'
+#
 #   Then move to more complex queries that expose other common cast members
 #       ./commCast.sh spring-tide the-sandhamn-murders the-team
+#
 #   Experiment to find the most useful results.
 #
 # EXAMPLES:
 #     ./commCast.sh murder-in blood-of-the-vine
-#     ./commCast.sh Brandvägg Jägarna "En man som heter Ove" "En pilgrims död"
+#     ./commCast.sh gasmamman kieler-street the-sandhamn-murders
 
 # Make sure we are in the correct directory
 DIRNAME=$(dirname "$0")
@@ -39,9 +45,6 @@ function cleanup() {
 # Get latest files to search
 BBOX="$(find BBox_TV_Credits*csv 2>/dev/null | tail -1)"
 MHZ="$(find MHz_TV_Credits*csv 2>/dev/null | tail -1)"
-IMDb="$(find IMDb_Credits-Person-noHype*csv 2>/dev/null | tail -1)"
-# Get latest files to use for translating show names
-XLATE="$(find IMDb-columns/xlate-pl-noHype*.txt 2>/dev/null | tail -1)"
 
 printf "==> Files processed:\n"
 #
@@ -59,23 +62,9 @@ else
     printf "MHZ:\tNo files match MHz_TV_Credits*csv\n"
 fi
 #
-if [ $IMDb ]; then
-    printf "IMDb:\t$IMDb\n"
-    foundFiles="yes"
-else
-    printf "IMDb:\tNo files match IMDb_Credits-Person-noHype*csv\n"
-fi
-#
 if [ "$foundFiles" != "yes" ]; then
     printf "==> [Error] No files available to process.\n"
     exit 1
-fi
-#
-if [ $XLATE ]; then
-    printf "XLATE:\t$XLATE\n"
-else
-    printf "XLATE:\tNo files match IMDb-columns/xlate-pl*.txt\n"
-    printf "\tNo translation will be done.\n"
 fi
 
 printf "\n==> Searching for:\n"
@@ -84,31 +73,17 @@ for a in "$@"; do
 done
 cat $SRCHFILE
 
-printf "\n==> All names (Name|Job|Show|Episode|Role):\n"
+printf "\n==> All names (Name|Job|Show|Role):\n"
 #
 if [ $BBOX ]; then
     if [ $(rg -wS -c -f $SRCHFILE $BBOX) ]; then
-        rg -wSIN --color always -f $SRCHFILE $BBOX |
-            awk -F "\t" '{printf ("%s\t%s\t%s\t\t%s\n", $1,$2,$4,$5)}' >>$TMPFILE
+        rg -f $SRCHFILE $BBOX | cut -f 1,2,4,5 >>$TMPFILE
     fi
 fi
 #
 if [ $MHZ ]; then
     if [ $(rg -wS -c -f $SRCHFILE $MHZ) ]; then
-        rg -wSIN --color always -f $SRCHFILE $MHZ |
-            awk -F "\t" '{printf ("%s\t%s\t%s\t\t%s\n", $1,$2,$4,$5)}' >>$TMPFILE
-    fi
-fi
-#
-if [ $IMDb ]; then
-    if [ $(rg -wS -c -f $SRCHFILE $IMDb) ]; then
-        if [ $XLATE ]; then
-            perl -p -f $XLATE $IMDb | rg -wSIN --color always -f $SRCHFILE |
-                awk -F "\t" '{printf ("%s\t%s\t%s\t%s\t%s\n", $1,$5,$2,$3,$6)}' >>$TMPFILE
-        else
-            rg -wSIN --color always -f $SRCHFILE $IMDb |
-                awk -F "\t" '{printf ("%s\t%s\t%s\t%s\t%s\n", $1,$5,$2,$3,$6)}' >>$TMPFILE
-        fi
+        rg -f $SRCHFILE $MHZ | cut -f 1,2,4,5 >>$TMPFILE
     fi
 fi
 
@@ -117,12 +92,15 @@ perl -pi -e "s+\t'+\t+g;" $TMPFILE
 # Print all search results
 TAB=$(printf "\t")
 sort -f --field-separator="$TAB" --key=1,1 --key=3,3 -fu $TMPFILE |
-    awk -F "\t" '{printf ("%-20s\t%-8s\t%-35s\t%-30s\t%-20s\n",$1,$2,$3,$4,$5)}'
+    rg -f $SRCHFILE | column -s $'\t' -t | rg -f $SRCHFILE
 
-printf "\n==> Duplicated names (Name|Job|Show|Episode|Role):\n"
+printf "\n==> Duplicated names (Name|Job|Show|Role):\n"
 # Spacing is sometimes erratic due to UTF-8 characters
 sort -f --field-separator="$TAB" --key=1,1 --key=3,3 -fu $TMPFILE |
-    awk -F "\t" 'BEGIN {p="%-20s\t%-8s\t%-35s\t%-30s\t%-20s\n"} {if($1==f[1]&&$3!=f[3])
-        {printf(p,f[1],f[2],f[3],f[4],f[5]); printf(p,$1,$2,$3,$4,$5)} split($0,f)}' | sort -fu
+    awk -F "\t" 'BEGIN {p="%s\t%s\t%s\t%s\t%s\n"}
+    {if($1==f[1]&&$3!=f[3])
+        {printf(p,f[1],f[2],f[3],f[4],f[5]); printf(p,$1,$2,$3,$4,$5)}
+        split($0,f)}' |
+    sort -fu | column -s $'\t' -t | rg -f $SRCHFILE
 
 rm -rf $TMPFILE $SRCHFILE
