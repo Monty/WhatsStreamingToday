06e135a42338858e0682354316e7f3a462fe0674
Monty Williams <monty.williams@gmail.com>
2024-04-13 17:05:51 -0700

    Only print episodes, not shows

--- newEpisodes.sh
+++ newEpisodes.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 #
 # diff the most recent TV_ShowsEpisodes files from MHz, Acorn, OPB, and Britbox
-# and print new, removed, or changed episodes
+# and print the new episodes
+#
+# Assume episodes have a season mumber in field 1, i.e. ', S[0-9][0-9]'
 
 # Make sure we are in the correct directory
 DIRNAME=$(dirname "$0")
@@ -21,22 +23,25 @@ MHZ_EPISODES_OLD=$(find MHz_TV_ShowsEpisodes-*csv | tail -2 | head -1)
 OPB_EPISODES=$(find OPB_TV_ShowsEpisodes-*csv | tail -1)
 OPB_EPISODES_OLD=$(find OPB_TV_ShowsEpisodes-*csv | tail -2 | head -1)
 
+# Default to finding new episodes
+TYPE="New"
+
 printf "==> $TYPE episodes in $MHZ_EPISODES\n"
 zet diff \
-    <(awk -f printTitles.awk "$MHZ_EPISODES") \
-    <(awk -f printTitles.awk "$MHZ_EPISODES_OLD")
+    <(cut -f 1 "$MHZ_EPISODES" | rg ', S[0-9][0-9]' | awk -f printTitles.awk) \
+    <(cut -f 1 "$MHZ_EPISODES_OLD" | rg ', S[0-9][0-9]' | awk -f printTitles.awk)
 waitUntil -kY "==> $TYPE episodes in $ACORN_EPISODES"
 
 zet diff \
-    <(awk -f printTitles.awk "$ACORN_EPISODES") \
-    <(awk -f printTitles.awk "$ACORN_EPISODES_OLD")
+    <(cut -f 1 "$ACORN_EPISODES" | rg ', S[0-9][0-9]' | awk -f printTitles.awk) \
+    <(cut -f 1 "$ACORN_EPISODES_OLD" | rg ', S[0-9][0-9]' | awk -f printTitles.awk)
 waitUntil -kY "==> $TYPE episodes in $OPB_EPISODES"
 
 zet diff \
-    <(awk -f printTitles.awk "$OPB_EPISODES") \
-    <(awk -f printTitles.awk "$OPB_EPISODES_OLD")
+    <(cut -f 1 "$OPB_EPISODES" | rg ', S[0-9][0-9]' | awk -f printTitles.awk) \
+    <(cut -f 1 "$OPB_EPISODES_OLD" | rg ', S[0-9][0-9]' | awk -f printTitles.awk)
 waitUntil -kY "==> $TYPE episodes in $BBOX_EPISODES"
 
 zet diff \
-    <(awk -f printTitles.awk "$BBOX_EPISODES") \
-    <(awk -f printTitles.awk "$BBOX_EPISODES_OLD")
+    <(cut -f 1 "$BBOX_EPISODES" | rg ', S[0-9][0-9]' | awk -f printTitles.awk) \
+    <(cut -f 1 "$BBOX_EPISODES_OLD" | rg ', S[0-9][0-9]' | awk -f printTitles.awk)
