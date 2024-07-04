cc9c913960563c131361504f1a9d6f2202bfa57a
Monty Williams <monty.williams@gmail.com>
2024-07-04 13:03:01 -0700

    Make newTitles.sh interactive

--- newTitles.sh
+++ newTitles.sh
@@ -6,6 +6,7 @@
 DIRNAME=$(dirname "$0")
 cd "$DIRNAME" || exit
 
+# shellcheck disable=SC1091 # waitUntil.function is a local file
 source waitUntil.function
 
 ACORN_TITLES=$(find Acorn_uniqTitles-*txt | tail -1)
@@ -21,28 +22,37 @@ OPB_TITLES=$(find OPB_uniqTitles-*txt | tail -1)
 OPB_TITLES_OLD=$(find OPB_uniqTitles-*txt | tail -2 | head -1)
 
 # First as a table with both new and removed
-waitUntil -k "==> New titles in $MHZ_TITLES"
-prettydiff "$MHZ_TITLES_OLD" "$MHZ_TITLES"
+printf "==> Show new titles in $MHZ_TITLES"
+if waitUntil -Y "?"; then
+    prettydiff "$MHZ_TITLES_OLD" "$MHZ_TITLES"
+fi
 
-waitUntil -k "==> New titles in $ACORN_TITLES"
-prettydiff "$ACORN_TITLES_OLD" "$ACORN_TITLES"
+printf "\n==> Show new titles in $ACORN_TITLES"
+if waitUntil -Y "?"; then
+    prettydiff "$ACORN_TITLES_OLD" "$ACORN_TITLES"
+fi
 
-waitUntil -k "==> New titles in $OPB_TITLES"
-prettydiff "$OPB_TITLES_OLD" "$OPB_TITLES"
+printf "\n==> Show new titles in $OPB_TITLES"
+if waitUntil -Y "?"; then
+    prettydiff "$OPB_TITLES_OLD" "$OPB_TITLES"
+fi
 
-waitUntil -k "==> New titles in $BBOX_TITLES"
-prettydiff "$BBOX_TITLES_OLD" "$BBOX_TITLES"
+printf "\n==> Show new titles in $BBOX_TITLES"
+if waitUntil -Y "?"; then
+    prettydiff "$BBOX_TITLES_OLD" "$BBOX_TITLES"
+fi
 
-waitUntil -k "==> All new titles as plain text"
-# Now just as plain text
-printf "\n==> New titles in $MHZ_TITLES\n"
-zet diff "$MHZ_TITLES" "$MHZ_TITLES_OLD"
+printf '\n==> Show All new titles as plain text'
+if waitUntil -Y "?"; then
+    printf "\nIn $MHZ_TITLES:\n"
+    zet diff "$MHZ_TITLES" "$MHZ_TITLES_OLD" | sed "s/^/    /g"
 
-printf "\n==> New titles in $ACORN_TITLES\n"
-zet diff "$ACORN_TITLES" "$ACORN_TITLES_OLD"
+    printf "\nIn $ACORN_TITLES:\n"
+    zet diff "$ACORN_TITLES" "$ACORN_TITLES_OLD" | sed "s/^/    /g"
 
-printf "\n==> New titles in $OPB_TITLES\n"
-zet diff "$OPB_TITLES" "$OPB_TITLES_OLD"
+    printf "\nIn $OPB_TITLES:\n"
+    zet diff "$OPB_TITLES" "$OPB_TITLES_OLD" | sed "s/^/    /g"
 
-printf "\n==> New titles in $BBOX_TITLES\n"
-zet diff "$BBOX_TITLES" "$BBOX_TITLES_OLD"
+    printf "\nIn $BBOX_TITLES:\n"
+    zet diff "$BBOX_TITLES" "$BBOX_TITLES_OLD" | sed "s/^/    /g"
+fi
