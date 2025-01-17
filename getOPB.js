47e47f4b5ece987c00b2fdea40d5fb801ebf8357
Monty Williams <monty.williams@gmail.com>
2025-01-16 19:43:06 -0800

    Update getOPB.js

--- getOPB.js
+++ getOPB.js
@@ -3,23 +3,33 @@ const fs = require('fs');
 
 const series_URL = process.env.TARGET;
 const output_file = process.env.RAW_HTML;
+// Access the TIMEOUT environment variable, defaulting to 2000 if not set
+const timeoutDuration = parseInt(process.env.TIMEOUT, 10) || 2000;
+console.log(`==> Timeout set to ${timeoutDuration}`);
 
 async function elementExists(page, role, ariaName) {
   const elementCount = await page.getByRole(role, { name: ariaName }).count();
   return elementCount > 0;
 }
 
-function appendToFile(tabData, showURL, filePath, content) {
+function appendToFile(headerInfo, showURL, filePath, content) {
   fs.appendFile(filePath, content, (err) => {
     if (err) {
       console.error(`==> Error writing to file ${filePath}:`, err);
     } else {
-      console.log(`==> Completed ${tabData} from ${showURL}`);
+      console.log(`==> Completed ${headerInfo} from ${showURL}`);
     }
   });
 }
 
-async function handleTabWithCombobox(page, tabName, writeDataFunction) {
+// Helper function to calculate the number of episodes
+function countEpisodes(tabContent) {
+  const falseEpisodes =
+    tabContent.split('paragraph: Providing Support for PBS.org').length - 1;
+  return tabContent.split('\n  - paragraph: ').length - 1 - falseEpisodes;
+}
+
+async function handleTabWithCombobox(page, tabName) {
   if (await elementExists(page, 'tab', tabName)) {
     await page.getByRole('tab', { name: tabName }).click();
 
@@ -52,12 +62,20 @@ async function handleTabWithCombobox(page, tabName, writeDataFunction) {
         await combobox.selectOption(option.value);
         // console.log(`Option ${option.label} was selected.`);
 
-        await page.waitForTimeout(2500); // Adjust time as needed
+        await page.waitForTimeout(timeoutDuration);
 
         const tabContent = await page
           .getByRole('tabpanel', { name: tabName })
           .ariaSnapshot();
-        await writeDataFunction(
+        const numberOfEpisodes = countEpisodes(tabContent);
+        // console.log(`    numberOfEpisodes =  ${numberOfEpisodes}`);
+        if (numberOfEpisodes == 0) {
+          console.warn(
+            `==> [Warning] ${numberOfEpisodes} episodes in ${tabName} tab "${option.label}" in`,
+            series_URL
+          );
+        }
+        await writeEpisodeData(
           page,
           `${tabName} tab "${option.label}" of "${numberOfSeasons}"`,
           tabContent
@@ -67,7 +85,15 @@ async function handleTabWithCombobox(page, tabName, writeDataFunction) {
       const tabContent = await page
         .getByRole('tabpanel', { name: tabName })
         .ariaSnapshot();
-      await writeDataFunction(page, `${tabName} tab`, tabContent);
+      const numberOfEpisodes = countEpisodes(tabContent);
+      // console.log(`    numberOfEpisodes =  ${numberOfEpisodes}`);
+      if (numberOfEpisodes == 0) {
+        console.warn(
+          `==> [Warning] ${numberOfEpisodes} episodes in ${tabName} in`,
+          series_URL
+        );
+      }
+      await writeEpisodeData(page, `${tabName} tab`, tabContent);
     }
   }
 }
@@ -79,7 +105,7 @@ async function handleTabWithCombobox(page, tabName, writeDataFunction) {
 //  - paragraph: S1 Ep10 | Vivian visits farms. (24m 40s)
 //  - button "Add to My List":
 
-async function writeEpisodeData(page, title, snapshot) {
+async function writeEpisodeData(page, headerInfo, snapshot) {
   const episodes = [];
   const linkCounts = new Map();
   const uniqueURLs = new Set();
@@ -135,7 +161,7 @@ async function writeEpisodeData(page, title, snapshot) {
       } else {
         console.error(
           `==> Link for "${currentLink}" in`,
-          `\n    ${title} not found in`,
+          `\n    ${headerInfo} not found in`,
           series_URL
         );
       }
@@ -155,23 +181,23 @@ async function writeEpisodeData(page, title, snapshot) {
     }
   }
   appendToFile(
-    title,
+    headerInfo,
     series_URL,
     output_file,
-    `<!-- ${title} data from ${series_URL} -->\n${episodes.join('\n')}\n\n`
+    `<!-- ${headerInfo} data from ${series_URL} -->\n${episodes.join('\n')}\n\n`
   );
 }
 
-async function writeEssentialData(title, source, eofString, offset) {
+async function writeEssentialData(headerInfo, source, eofString, offset) {
   let essentialData = '';
   const splitLines = source.split('\n');
   const firstIndex = splitLines.indexOf(eofString);
   essentialData = splitLines.slice(0, firstIndex + offset).join('\n');
   appendToFile(
-    title,
+    headerInfo,
     series_URL,
     output_file,
-    `<!-- ${title} data from ${series_URL} -->\n${essentialData}\n\n`
+    `<!-- ${headerInfo} data from ${series_URL} -->\n${essentialData}\n\n`
   );
 }
 
@@ -215,13 +241,13 @@ removeFile(output_file);
     }
 
     // Get episodes from the Clips & Previews tab
-    await handleTabWithCombobox(page, 'Clips & Previews', writeEpisodeData);
+    await handleTabWithCombobox(page, 'Clips & Previews');
 
     // Get episodes from the Special tab
-    await handleTabWithCombobox(page, 'Special', writeEpisodeData);
+    await handleTabWithCombobox(page, 'Special');
 
     // Get episodes from the Episodes tab
-    await handleTabWithCombobox(page, 'Episodes', writeEpisodeData);
+    await handleTabWithCombobox(page, 'Episodes');
   } catch (error) {
     if (error.name === 'TimeoutError') {
       console.error('==> Page load timed out for', series_URL);
