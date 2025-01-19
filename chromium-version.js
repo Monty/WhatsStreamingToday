85d76e0e41d7d7f4507ccf952c1a8b0fad91ab34
Monty Williams <monty.williams@gmail.com>
2025-01-19 10:30:54 -0800

    Create chromium-version.js

---
+++ chromium-version.js
@@ -0,0 +1,13 @@
+const { chromium } = require('playwright');
+
+(async () => {
+  const browser = await chromium.launch({
+    headless: true,
+  });
+
+  const version = await browser.version();
+
+  console.log('Chromium version:', version);
+
+  await browser.close();
+})();
