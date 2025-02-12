23f3b32bff13a768f1b0dbcb4f3ed55f424fa014
Monty Williams <monty.williams@gmail.com>
2025-02-12 00:13:15 -0800

    After runnning prettier | eslint --fix on .js files

--- getOPB.js
+++ getOPB.js
@@ -1,5 +1,11 @@
-const { chromium } = require('playwright');
-const fs = require('fs');
+/* eslint capitalized-comments: "off", no-magic-numbers: "off"
+    --------
+    Comments are debugging console.log statements
+    Magic numbers are mostly lengths and offsets
+  */
+
+const { chromium } = require("playwright");
+const fs = require("fs");
 
 const series_URL = process.env.TARGET;
 const output_file = process.env.RAW_HTML;
@@ -25,8 +31,8 @@ function appendToFile(headingTitle, showURL, filePath, sectionHeading) {
 // Helper function to calculate the number of episodes in a season
 function countEpisodes(tabData) {
   const falseEpisodes =
-    tabData.split('paragraph: Providing Support for PBS.org').length - 1;
-  return tabData.split('\n  - paragraph: ').length - 1 - falseEpisodes;
+    tabData.split("paragraph: Providing Support for PBS.org").length - 1;
+  return tabData.split("\n  - paragraph: ").length - 1 - falseEpisodes;
 }
 
 async function handleTab(page, tabName) {
@@ -35,7 +41,7 @@ async function handleTab(page, tabName) {
     if (season && season.label) {
       seasonName = `"${season.label}" `;
     } else {
-      seasonName = '';
+      seasonName = "";
     }
     // console.log(`tabName = ${tabName}`);
     // console.log(`seasonName = ${seasonName}`);
@@ -45,7 +51,7 @@ async function handleTab(page, tabName) {
     let retries = 0;
     while (retries < maxRetries) {
       seasonContent = await page
-        .getByRole('tabpanel', { name: tabName })
+        .getByRole("tabpanel", { name: tabName })
         .ariaSnapshot();
       const numberOfEpisodes = countEpisodes(seasonContent);
       // console.log(`    numberOfEpisodes =  ${numberOfEpisodes}`);
@@ -56,17 +62,19 @@ async function handleTab(page, tabName) {
           await page.waitForTimeout(500);
         } else {
           console.error(
-            `==> [Error] ${numberOfEpisodes} episodes in ${tabName} tab ${seasonName}after ${maxRetries} retries in`,
-            series_URL
+            `==> [Error] ${numberOfEpisodes} episodes in ${tabName} ` +
+              `tab ${seasonName} after ${maxRetries} retries in`,
+            series_URL,
           );
         }
       } else {
         if (retries > 0) {
-          const episodeText = numberOfEpisodes !== 1 ? 'episodes' : 'episode';
-          const retriesText = retries !== 1 ? 'retries' : 'retry';
+          const episodeText = numberOfEpisodes !== 1 ? "episodes" : "episode";
+          const retriesText = retries !== 1 ? "retries" : "retry";
           console.warn(
-            `==> [Warning] Finding ${numberOfEpisodes} ${episodeText} in ${tabName} tab ${seasonName}took ${retries} ${retriesText} in`,
-            series_URL
+            `==> [Warning] Finding ${numberOfEpisodes} ${episodeText} in ` +
+              `${tabName} tab ${seasonName}took ${retries} ${retriesText} in`,
+            series_URL,
           );
         }
         break; // Exit loop if episodes are found
@@ -75,33 +83,33 @@ async function handleTab(page, tabName) {
     return seasonContent;
   }
 
-  if (await elementExists(page, 'tab', tabName)) {
-    await page.getByRole('tab', { name: tabName }).click();
+  if (await elementExists(page, "tab", tabName)) {
+    await page.getByRole("tab", { name: tabName }).click();
 
     // Is there a combobox in this tab?
-    const combobox = await page.getByRole('combobox').first();
+    const combobox = await page.getByRole("combobox").first();
     if (await combobox.isVisible()) {
       // There is a combobox
       const options = await combobox.evaluate((select) =>
         Array.from(select.options).map((option) => ({
           value: option.value,
           label: option.textContent.trim(),
-        }))
+        })),
       );
-      numberOfSeasons = options.length;
+      const numberOfSeasons = options.length;
 
       for (const option of options) {
         // Select the option
         await combobox.selectOption(option.value);
         // Wait for the selected option to have the "selected" attribute
         const selectedOption = combobox.locator(
-          `option[value="${option.value}"]`
+          `option[value="${option.value}"]`,
         );
         const isSelected = await selectedOption.evaluate((el) => el.selected);
         if (!isSelected) {
           console.error(
             `==> Option "${option.label}" in ${tabName} was not selected in`,
-            series_URL
+            series_URL,
           );
         }
 
@@ -119,7 +127,7 @@ async function handleTab(page, tabName) {
         await writeOneSeasonsEpisodesData(
           page,
           `${tabName} tab "${option.label}" of "${numberOfSeasons}"`,
-          currentSeason
+          currentSeason,
         );
       }
     } else {
@@ -133,7 +141,7 @@ async function handleTab(page, tabName) {
 // The sequence defining an episode
 //  - paragraph:
 //    - link "Love Me Some Candied Yams"
-//or  - 'link "Love Me Some Candied Yams"'
+// or  - 'link "Love Me Some Candied Yams"'
 //  - paragraph: S1 Ep10 | Vivian visits farms. (24m 40s)
 //  - button "Add to My List":
 
@@ -141,17 +149,17 @@ async function writeOneSeasonsEpisodesData(page, headingTitle, snapshot) {
   const episodes = [];
   const linkCounts = new Map();
   const uniqueURLs = new Set();
-  const lines = snapshot.split('\n');
+  const lines = snapshot.split("\n");
   let isInParagraph = false;
-  let currentLink = '';
+  let currentLink = "";
 
   for (const line of lines) {
-    if (line === '  - paragraph:') {
+    if (line === "  - paragraph:") {
       isInParagraph = true;
       continue;
     }
 
-    if (isInParagraph && line.match(/^    - (?:')?link /)) {
+    if (isInParagraph && line.match(/^ {4}- (?:')?link /)) {
       episodes.push(line);
 
       // Extract currentLink based on the line format
@@ -161,7 +169,7 @@ async function writeOneSeasonsEpisodesData(page, headingTitle, snapshot) {
         currentLink = line.substring(13, line.length - 2);
       }
       currentLink = currentLink.replace(/\\"/g, '"');
-      currentLink = currentLink.replace(/\'\'/g, "'");
+      currentLink = currentLink.replace(/''/g, "'");
       // console.log('\n==> linkName:', currentLink);
 
       // Track the occurrences of currentLink
@@ -172,12 +180,12 @@ async function writeOneSeasonsEpisodesData(page, headingTitle, snapshot) {
 
       // Fetch all links matching the currentLink
       const ariaLinks = await page
-        .getByRole('link', { name: currentLink, exact: true })
+        .getByRole("link", { name: currentLink, exact: true })
         .all();
       // console.log('==> ariaLinks:', ariaLinks);
 
       for (const link of ariaLinks) {
-        const aSingleURL = await link.getAttribute('href');
+        const aSingleURL = await link.getAttribute("href");
         if (aSingleURL) {
           uniqueURLs.add(aSingleURL);
         }
@@ -194,21 +202,21 @@ async function writeOneSeasonsEpisodesData(page, headingTitle, snapshot) {
         console.error(
           `==> Link for "${currentLink}" in`,
           `\n    ${headingTitle} not found in`,
-          series_URL
+          series_URL,
         );
       }
     }
 
-    if (isInParagraph && line.startsWith('  - paragraph: ')) {
+    if (isInParagraph && line.startsWith("  - paragraph: ")) {
       episodes.push(line);
       // console.log('==> paragraph:', line);
     }
 
-    if (isInParagraph && line.startsWith('  - button')) {
+    if (isInParagraph && line.startsWith("  - button")) {
       isInParagraph = false;
       // console.log('==> uniqueURLs:', uniqueURLs);
       // console.log('==> button:', line);
-      currentLink = '';
+      currentLink = "";
       uniqueURLs.clear();
     }
   }
@@ -216,27 +224,28 @@ async function writeOneSeasonsEpisodesData(page, headingTitle, snapshot) {
     headingTitle,
     series_URL,
     output_file,
-    `<!-- ${headingTitle} data from ${series_URL} -->\n${episodes.join('\n')}\n\n`
+    `<!-- ${headingTitle} data from ${series_URL} ` +
+      `-->\n${episodes.join("\n")}\n\n`,
   );
 }
 
 async function writeEssentialData(headingTitle, snapshot, eofString, offset) {
-  let essentialData = '';
-  const splitLines = snapshot.split('\n');
+  let essentialData = "";
+  const splitLines = snapshot.split("\n");
   const firstIndex = splitLines.indexOf(eofString);
-  essentialData = splitLines.slice(0, firstIndex + offset).join('\n');
+  essentialData = splitLines.slice(0, firstIndex + offset).join("\n");
   appendToFile(
     headingTitle,
     series_URL,
     output_file,
-    `<!-- ${headingTitle} data from ${series_URL} -->\n${essentialData}\n\n`
+    `<!-- ${headingTitle} data from ${series_URL} -->\n${essentialData}\n\n`,
   );
 }
 
 function removeFile(filePath) {
   fs.unlink(filePath, (err) => {
-    if (err && err.code !== 'ENOENT') {
-      console.error('==> Error deleting file: ' + filePath, err);
+    if (err && err.code !== "ENOENT") {
+      console.error("==> Error deleting file: " + filePath, err);
     }
   });
 }
@@ -249,43 +258,43 @@ removeFile(output_file);
     headless: true,
   });
   const context = await browser.newContext({
-    storageState: 'auth/cookies.json',
+    storageState: "auth/cookies.json",
   });
   const page = await context.newPage();
-  console.log('\n==> Processing', series_URL);
+  console.log("\n==> Processing", series_URL);
 
   try {
     await page.goto(series_URL, { timeout: 30000 });
-    await page.waitForTimeout(10000); // wait for 10 seconds
+    await page.waitForTimeout(10000); // Wait for 10 seconds
 
     // 1) Get the top level page
-    const mainPage = await page.locator('#maincontent').ariaSnapshot();
-    writeEssentialData('Main page', mainPage, '  - tablist:', 0);
+    const mainPage = await page.locator("#maincontent").ariaSnapshot();
+    writeEssentialData("Main page", mainPage, "  - tablist:", 0);
 
     // 2) Get the Genre from the About tab, which should always exist
-    if (await elementExists(page, 'tab', 'About')) {
-      await page.getByRole('tab', { name: 'About' }).click();
+    if (await elementExists(page, "tab", "About")) {
+      await page.getByRole("tab", { name: "About" }).click();
       const aboutTab = await page
-        .getByRole('tabpanel', { name: 'About' })
+        .getByRole("tabpanel", { name: "About" })
         .ariaSnapshot();
-      writeEssentialData('About tab', aboutTab, '    - listitem:', 2);
+      writeEssentialData("About tab", aboutTab, "    - listitem:", 2);
     } else {
       console.error('==> The "About" tab does not exist in', series_URL);
     }
 
     // 3) Get episodes from the Clips & Previews tab
-    await handleTab(page, 'Clips & Previews');
+    await handleTab(page, "Clips & Previews");
 
     // 4) Get episodes from the Special tab
-    await handleTab(page, 'Special');
+    await handleTab(page, "Special");
 
     // 5) Get episodes from the Episodes tab
-    await handleTab(page, 'Episodes');
+    await handleTab(page, "Episodes");
   } catch (error) {
-    if (error.name === 'TimeoutError') {
-      console.error('==> Page load timed out for', series_URL);
+    if (error.name === "TimeoutError") {
+      console.error("==> Page load timed out for", series_URL);
     } else {
-      console.error('==> Error during page navigation:', error.message);
+      console.error("==> Error during page navigation:", error.message);
     }
   } finally {
     await context.close();
@@ -293,7 +302,7 @@ removeFile(output_file);
   }
 })();
 
-process.on('unhandledRejection', (reason, promise) => {
-  console.error('==> Unhandled Rejection at:', promise, 'reason:', reason);
+process.on("unhandledRejection", (reason, promise) => {
+  console.error("==> Unhandled Rejection at:", promise, "reason:", reason);
   process.exit(1);
 });
