06e3b9811f66bf16047beafb310978559e1cd259
Monty Williams <monty.williams@gmail.com>
2025-02-15 23:29:12 -0800

    Fix broken saveTodays*Files.sh

--- saveTodaysBBoxFiles.sh
+++ saveTodaysBBoxFiles.sh
@@ -2,6 +2,8 @@
 # Save the current days results as a baseline so we can check for changes in the future
 # -d DATE picks a different date
 # -v does verbose copying
+#
+# shellcheck disable=SC2086
 
 # Make sure we are in the correct directory
 DIRNAME=$(dirname "$0")
@@ -33,11 +35,11 @@ COLS="BBox-columns"
 BASELINE="BBox-baseline"
 mkdir -p $COLS $BASELINE
 
-cp -p "$VERBOSE" BBox_TV_Credits-"$DATE".csv $BASELINE/credits.txt
-cp -p "$VERBOSE" BBox_TV_Shows-"$DATE".csv $BASELINE/spreadsheet.txt
-cp -p "$VERBOSE" BBox_TV_ShowsEpisodes-"$DATE".csv $BASELINE/spreadsheetEpisodes.txt
-cp -p "$VERBOSE" BBox_uniqCharacters-"$DATE".txt $BASELINE/uniqCharacters.txt
-cp -p "$VERBOSE" BBox_uniqPersons-"$DATE".txt $BASELINE/uniqPersons.txt
-cp -p "$VERBOSE" BBox_uniqTitles-"$DATE".txt $BASELINE/uniqTitles.txt
+cp -p $VERBOSE BBox_TV_Credits-"$DATE".csv $BASELINE/credits.txt
+cp -p $VERBOSE BBox_TV_Shows-"$DATE".csv $BASELINE/spreadsheet.txt
+cp -p $VERBOSE BBox_TV_ShowsEpisodes-"$DATE".csv $BASELINE/spreadsheetEpisodes.txt
+cp -p $VERBOSE BBox_uniqCharacters-"$DATE".txt $BASELINE/uniqCharacters.txt
+cp -p $VERBOSE BBox_uniqPersons-"$DATE".txt $BASELINE/uniqPersons.txt
+cp -p $VERBOSE BBox_uniqTitles-"$DATE".txt $BASELINE/uniqTitles.txt
 
-cp -p "$VERBOSE" $COLS/all_URLs-"$DATE".txt $BASELINE/all_URLs.txt
+cp -p $VERBOSE $COLS/all_URLs-"$DATE".txt $BASELINE/all_URLs.txt
