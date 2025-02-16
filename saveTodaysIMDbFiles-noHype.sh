4d9d2cc5b6c96312644645102f789781e92d4f5a
Monty Williams <monty.williams@gmail.com>
2025-02-15 18:00:54 -0800

    shellcheck more files

--- saveTodaysIMDbFiles-noHype.sh
+++ saveTodaysIMDbFiles-noHype.sh
@@ -5,7 +5,7 @@
 
 # Make sure we are in the correct directory
 DIRNAME=$(dirname "$0")
-cd $DIRNAME
+cd "$DIRNAME" || exit
 
 # Create a timestamp
 DATE="$(date +%y%m%d)"
@@ -34,22 +34,22 @@ BASELINE="IMDb-baseline"
 mkdir -p $COLS $BASELINE
 
 # Manually maintained skip episodes file
-cp -p $VERBOSE skipEpisodes.TCONST $BASELINE/skipEpisodes.TCONST
+cp -p "$VERBOSE" skipEpisodes.TCONST $BASELINE/skipEpisodes.TCONST
 
 # Copy -noHype files
-cp -p $VERBOSE $COLS/tconst_all-noHype-$DATE.txt $BASELINE/tconst_all-noHype.txt
-cp -p $VERBOSE $COLS/tconst-noHype-$DATE.txt $BASELINE/tconst-noHype.txt
-cp -p $VERBOSE $COLS/nconst-noHype-$DATE.txt $BASELINE/nconst-noHype.txt
-cp -p $VERBOSE $COLS/raw_shows-noHype-$DATE.csv $BASELINE/raw_shows-noHype.csv
-cp -p $VERBOSE $COLS/raw_persons-noHype-$DATE.csv $BASELINE/raw_persons-noHype.csv
-cp -p $VERBOSE $COLS/tconst_known-noHype-$DATE.txt $BASELINE/tconst_known-noHype.txt
-cp -p $VERBOSE $COLS/tconst-episodes-noHype-$DATE.txt $BASELINE/tconst-episodes-noHype.csv
+cp -p "$VERBOSE" $COLS/tconst_all-noHype-"$DATE".txt $BASELINE/tconst_all-noHype.txt
+cp -p "$VERBOSE" $COLS/tconst-noHype-"$DATE".txt $BASELINE/tconst-noHype.txt
+cp -p "$VERBOSE" $COLS/nconst-noHype-"$DATE".txt $BASELINE/nconst-noHype.txt
+cp -p "$VERBOSE" $COLS/raw_shows-noHype-"$DATE".csv $BASELINE/raw_shows-noHype.csv
+cp -p "$VERBOSE" $COLS/raw_persons-noHype-"$DATE".csv $BASELINE/raw_persons-noHype.csv
+cp -p "$VERBOSE" $COLS/tconst_known-noHype-"$DATE".txt $BASELINE/tconst_known-noHype.txt
+cp -p "$VERBOSE" $COLS/tconst-episodes-noHype-"$DATE".txt $BASELINE/tconst-episodes-noHype.csv
 
-cp -p $VERBOSE IMDb_uniqTitles-noHype-$DATE.txt $BASELINE/uniqTitles-noHype.txt
-cp -p $VERBOSE IMDb_uniqPersons-noHype-$DATE.txt $BASELINE/uniqPersons-noHype.txt
+cp -p "$VERBOSE" IMDb_uniqTitles-noHype-"$DATE".txt $BASELINE/uniqTitles-noHype.txt
+cp -p "$VERBOSE" IMDb_uniqPersons-noHype-"$DATE".txt $BASELINE/uniqPersons-noHype.txt
 
-cp -p $VERBOSE IMDb_Shows-noHype-$DATE.csv $BASELINE/shows-noHype.csv
-cp -p $VERBOSE IMDb_Credits-Show-noHype-$DATE.csv $BASELINE/credits-show-noHype.csv
-cp -p $VERBOSE IMDb_Credits-Person-noHype-$DATE.csv $BASELINE/credits-person-noHype.csv
-cp -p $VERBOSE IMDb_Persons-Titles-noHype-$DATE.csv $BASELINE/persons-titles-noHype.csv
-cp -p $VERBOSE IMDb_associatedTitles-noHype-$DATE.csv $BASELINE/associatedTitles-noHype.csv
+cp -p "$VERBOSE" IMDb_Shows-noHype-"$DATE".csv $BASELINE/shows-noHype.csv
+cp -p "$VERBOSE" IMDb_Credits-Show-noHype-"$DATE".csv $BASELINE/credits-show-noHype.csv
+cp -p "$VERBOSE" IMDb_Credits-Person-noHype-"$DATE".csv $BASELINE/credits-person-noHype.csv
+cp -p "$VERBOSE" IMDb_Persons-Titles-noHype-"$DATE".csv $BASELINE/persons-titles-noHype.csv
+cp -p "$VERBOSE" IMDb_associatedTitles-noHype-"$DATE".csv $BASELINE/associatedTitles-noHype.csv
