08145547d09d89deabfca229fec93dbaa62be5b4
Monty Williams <monty.williams@gmail.com>
2025-02-15 17:30:09 -0800

    shellcheck countIMDbInstances.sh

--- countIMDbInstances.sh
+++ countIMDbInstances.sh
@@ -9,10 +9,10 @@
 for file in title.basics.tsv.gz title.akas.tsv.gz title.principals.tsv.gz title.crew.tsv.gz \
     title.episode.tsv.gz name.basics.tsv.gz; do
     if [ -e "$file" ]; then
-        count=$(rg -wScz $1 $file)
+        count=$(rg -wScz "$1" $file)
         if [ "$count" == "" ]; then
             count=0
         fi
-        printf "%-10s\t%5d\t%s\n" $1 $count $file
+        printf "%-10s\t%5d\t%s\n" "$1" "$count" $file
     fi
 done
