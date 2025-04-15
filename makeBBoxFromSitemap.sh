04f28d0a72e8e0a01b208002c457632409e29f2a
Monty Williams <monty.williams@gmail.com>
2025-04-14 18:00:24 -0700

    Switch from "which curl" to "command -v curl"

--- makeBBoxFromSitemap.sh
+++ makeBBoxFromSitemap.sh
@@ -43,7 +43,7 @@ while getopts ":dst" opt; do
 done
 
 # Make sure we can execute curl.
-if [ ! -x "$(which curl 2>/dev/null)" ]; then
+if ! command -v curl >/dev/null; then
     printf "[Error] Can't run curl. Install curl and rerun this script.\n"
     printf "        To test, type:  curl -Is https://github.com/ | head -5\n"
     exit 1
