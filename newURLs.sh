0054b9f0f42dd2810722ba52370a11481fd8f9cc
Monty Williams <monty.williams@gmail.com>
2025-06-08 15:22:54 -0700

    Use new_episode_urls.txt instead of latest existing
    Breaks until new_episode_urls.txt is generated

--- newURLs.sh
+++ newURLs.sh
@@ -34,38 +34,35 @@ while getopts ":b:" opt; do
     esac
 done
 
-# Default to using the second oldest release
-BACK="${BACK:-2}"
-if [[ $BACK -lt 2 ]]; then
-    BACK=2
-fi
+# Default to using the newest release
+BACK="${BACK:-1}"
 
 # shellcheck disable=SC1091 # waitUntil.function is a local file
 source waitUntil.function
 
 #    Acorn-columns/episode_urls-250603.txt
-#    - Acorn-columns/show_urls-250603.txt
-ACORN_URLS=$(find Acorn-columns/episode_urls-*.txt | tail -1)
+ACORN_URLS=Acorn-columns/new_episode_urls.txt
 ACORN_URLS_OLD=$(find Acorn-columns/episode_urls-*.txt | tail -"$BACK" | head -1)
 #
 #    BBox-columns/all_URLs-250603.txt
-BBOX_URLS=$(find BBox-columns/all_URLs-*.txt | tail -1)
+BBOX_URLS=BBox-columns/new_episode_urls.txt
 BBOX_URLS_OLD=$(find BBox-columns/all_URLs-*.txt | tail -"$BACK" | head -1)
 #
 #    MHz-columns/MHz_urls-250603.txt
 #    - MHz-columns/episode_urls-250603.txt
 #    - MHz-columns/movie_urls-250603.txt
 #    - MHz-columns/season_urls-250603.txt
-MHZ_URLS=$(find MHz-columns/MHz_urls-*.txt | tail -1)
+MHZ_URLS=MHz-columns/new_episode_urls.txt
 MHZ_URLS_OLD=$(find MHz-columns/MHz_urls-*.txt | tail -"$BACK" | head -1)
 #
 #    OPB-columns/show_urls-250603.txt
 #    - OPB-columns/raw_data-250603.txt
-OPB_URLS=$(find OPB-columns/show_urls-*.txt | tail -1)
+OPB_URLS=OPB-columns/new_episode_urls.txt
 OPB_URLS_OLD=$(find OPB-columns/show_urls-*.txt | tail -"$BACK" | head -1)
 
 printf "\n==> Show new URLs since $MHZ_URLS_OLD"
 if waitUntil -Y "?"; then
+    rm -f "$MHZ_URLS"
     zet diff "$MHZ_URLS" "$MHZ_URLS_OLD" |
         sd "https://watch.mhzchoice.com/" "" |
         rg -v "^coming-soon/|-available-"
@@ -73,18 +70,21 @@ fi
 
 printf "\n==> Show new URLs since $ACORN_URLS_OLD"
 if waitUntil -Y "?"; then
+    rm -f "$ACORN_URLS"
     zet diff "$ACORN_URLS" "$ACORN_URLS_OLD" |
         sd "https://acorn.tv/" ""
 fi
 
 printf "\n==> Show new URLs since $OPB_URLS_OLD"
 if waitUntil -Y "?"; then
+    rm -f "$OPB_URLS"
     zet diff "$OPB_URLS" "$OPB_URLS_OLD" |
         sd "https://www.pbs.org/" ""
 fi
 
 printf "\n==> Show new URLs since $BBOX_URLS_OLD"
 if waitUntil -Y "?"; then
+    rm -f "$BBOX_URLS"
     zet diff "$BBOX_URLS" "$BBOX_URLS_OLD" |
         sd "https://www.britbox.com/(us|ca)/" "" | rg "^episode/" |
         rg -v "Casualty_|Coming_Soon_|Coronation_Street_|Eastenders_" |
