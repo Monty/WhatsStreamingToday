8728f73fe76e1e2aa310df017c994c09c8799437
Monty Williams <monty.williams@gmail.com>
2026-02-14 15:28:58 -0800

    Change way Acorn ignores collections

--- makeAcornFromBrowsePage.sh
+++ makeAcornFromBrowsePage.sh
@@ -126,7 +126,7 @@ if [ ! -e "$SHOW_URLS" ]; then
     printf "==> Downloading new $SHOW_URLS\n"
     curl -sS $BROWSE_URL | grep '<a itemprop="url"' |
         sed -e 's+.*http+http+' -e 's+/">$++' |
-        rg -v 'janeandrohan|secretsandsports' | sort -f >"$SHOW_URLS"
+        rg -v -f rg_Acorn_skip.rgx | sort -f >"$SHOW_URLS"
 else
     printf "==> Using existing $SHOW_URLS\n"
 fi
